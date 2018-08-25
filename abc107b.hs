6c79a4bf9a37854230b0697998542e22770306f0
4afs <g2x649@gmail.com>
2018-08-25 23:09:20 +0000

    correct variable name

--- abc107b.hs
+++ abc107b.hs
@@ -3,18 +3,18 @@ import Control.Monad
 main = do
     (h:w:_) <- map read . words <$> getLine :: IO [Int]
     xs <- replicateM h getLine
-    let a1 = delAllDotLine h xs
-        a2 = delAllDotLine w $ spin (length (head a1)) a1
-    mapM_ putStrLn $ spin (length (head a2)) a2
+    mapM_ putStrLn . transpose . compress w . transpose $ compress h xs
 
-delAllDotLine :: Int -> [String] -> [String]
-delAllDotLine _ [] = []
-delAllDotLine 0 _ = []
-delAllDotLine n (x:xs)
-  | all (=='.') x = delAllDotLine (n-1) xs
-  | otherwise = x : delAllDotLine (n-1) xs
+compress :: Int -> [String] -> [String]
+compress 0 _ = []
+compress _ [] = []
+compress n (x:xs)
+  | all (=='.') x = compress (n-1) xs
+  | otherwise = x : compress (n-1) xs
 
-spin :: Int -> [String] -> [String]
-spin 0 _ = []
-spin n xs = map head xs : spin (n-1) (map tail xs)
+transpose :: [String] -> [String]
+transpose xs = transpose' (length (head xs)) xs
+    where 
+        transpose' 0 _ = []
+        transpose' n xs = map head xs : transpose' (n-1) (map tail xs)
 
