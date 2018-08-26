a031eddeb23c3654afbb4373a01fceaf7a18a39a
4afs <g2x649@gmail.com>
2018-08-26 02:02:36 +0000

    to one-line

--- abc107b.hs
+++ abc107b.hs
@@ -1,16 +1,6 @@
 import Control.Monad
-
-main = do
-    (h:_:_) <- map read . words <$> getLine :: IO [Int]
-    xs <- replicateM h getLine
-    mapM_ putStrLn . transpose . compress . transpose $ compress xs
-
-compress :: [String] -> [String]
-compress = filter(\x -> '#' `elem` x)
-
-transpose :: [String] -> [String]
-transpose xs = transpose' (length (head xs)) xs
-    where 
-        transpose' 0 _ = []
-        transpose' n xs = map head xs : transpose' (n-1) (map tail xs)
-
+import Data.List 
+main = 
+    head . map read . words <$> getLine >>=
+        flip replicateM getLine >>=
+            mapM_ putStrLn . transpose . filter (\x -> '#' `elem` x) . transpose . filter (\x -> '#' `elem` x)
