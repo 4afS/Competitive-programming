197345cc5642e2c9741a76a361d9f24b20ca5985
4afs <g2x649@gmail.com>
2018-09-04 19:16:28 +0000

    rename files to shorter

--- test.hs
+++
@@ -1,6 +0,0 @@
-import Debug.Trace
-
-main = print $ factorial 1 10
-factorial :: Integer -> Integer -> Integer
-factorial n 0 = n
-factorial n count = trace ("n = " ++ show n) factorial (n*count) (count-1)
